49798be9031be856e8731f98addd0f7e4a686da7
imedina <imedina@cipf.es>
2009-07-06 14:19:47 +0200

    0.9.0 beta

--- src/main/java/org/bioinfo/infrared/InfraredMain.java
+++ src/main/java/org/bioinfo/infrared/InfraredMain.java
@@ -1,8 +1,22 @@
 package org.bioinfo.infrared;
 
+import org.bioinfo.infrared.tools.InfraredFactory;
+import org.bioinfo.infrared.tools.InfraredTool;
+
+
 public class InfraredMain {
 
-	public static void main() {
+	public static void main(String[] args) {
+		
+		
+		if(args.length == 0) {
+			System.out.println("No options provided");
+		}
+		
+		InfraredTool tool = InfraredFactory.createTool(args);
+		if(tool != null) {
+			tool.execute();
+		}
 		
 	}
 	
