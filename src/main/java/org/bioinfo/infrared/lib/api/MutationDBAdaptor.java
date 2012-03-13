08b4ea017c20f41620a1ff743bd617af8a25c8aa
Francisco Salavert Torres <frasator@gmail.com>
2012-03-13 12:55:47 +0100

    conserved region using feature map and mutations

---
+++ src/main/java/org/bioinfo/infrared/lib/api/MutationDBAdaptor.java
@@ -0,0 +1,13 @@
+package org.bioinfo.infrared.lib.api;
+
+import java.util.List;
+
+
+import org.bioinfo.infrared.core.cellbase.MutationPhenotypeAnnotation;
+import org.bioinfo.infrared.lib.common.Region;
+
+public interface MutationDBAdaptor {
+	public List<MutationPhenotypeAnnotation> getAllByRegion(Region region);
+	public List<List<MutationPhenotypeAnnotation>> getAllByRegionList(List<Region> regionList);
+
+}
