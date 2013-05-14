fae7f0c26e70f29fc21785105dbdb6cb606c4e2f
imedina <imedina@cipf.es>
2013-05-14 14:24:46 +0200

    Variation: Java Beans copied from cellbase project

---
+++ src/main/java/org/bioinfo/cellbase/lib/common/variation/Phenotype.java
@@ -0,0 +1,14 @@
+package org.bioinfo.cellbase.lib.common.variation;
+
+public class Phenotype {
+
+	private String associated_variant_risk_allele = null;
+	private String risk_allele_freq_in_controls = null;
+	private String p_value = null;
+	private String associated_gene = null;
+	private String name = null;
+	private String description = null;
+	private String source = null;
+	private String version = null;
+	private String repository = null;
+}
---
+++ src/main/java/org/bioinfo/cellbase/lib/common/variation/SampleGenotype.java
@@ -0,0 +1,8 @@
+package org.bioinfo.cellbase.lib.common.variation;
+
+public class SampleGenotype {
+	private String genotype;
+	private String zygosity;
+	private String total_reads;
+
+}
